4041301838ab4d42d05cf9cc52862ad28846ecbf
Debu <debajyoti.d.mukherjee@gmail.com>
2020-06-09 21:58:49 +0530

    changed

--- movies/src/main/java/com/redhat/movies/Movie.java
+++ movies/src/main/java/com/redhat/movies/Movie.java
@@ -2,12 +2,17 @@ package com.redhat.movies;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Movie implements Serializable {
 
     private static final long serialVersionUID = -3240337073623122124L;
 
+    @JsonProperty
     private Integer movieId;
+    @JsonProperty
     private String name;
+    @JsonProperty
     private String genre;
 
     public Movie(Integer movieId, String name, String genre) {
